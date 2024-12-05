af514b2bc0320326c15febe0da10577581ccb1e9
RammaruIshii <ranran878@i.softbank.jp>
2024-12-05 21:00:15 +0900

    環境ごとにエンドポイントの出し分けを確認

--- MyAPIApp/Views/ContentView.swift
+++ MyAPIApp/Views/ContentView.swift
@@ -8,6 +8,7 @@
 import SwiftUI
 
 struct ContentView: View {
+    let apiUrl: String = "API_URL"
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -16,6 +17,12 @@ struct ContentView: View {
             Text("Hello, world!")
         }
         .padding()
+        .onAppear {
+            if let apiUrl = Bundle.main.object(forInfoDictionaryKey: apiUrl) as? String {
+                // 環境ごとにエンドポイントが出し分けできているか確認
+                print("API URL: \(apiUrl)")
+            }
+        }
     }
 }
 
