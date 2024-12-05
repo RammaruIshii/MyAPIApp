704ef095ecbcd3f078ddc9542f12cb65261ed2a7
RammaruIshii <ranran878@i.softbank.jp>
2024-12-05 17:56:21 +0900

    iOSプロジェクトを作成

---
+++ MyAPIApp/MyAPIAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyAPIAppApp.swift
+//  MyAPIApp
+//
+//  Created by 石井蘭丸 on 2024/12/05.
+//
+
+import SwiftUI
+
+@main
+struct MyAPIAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
